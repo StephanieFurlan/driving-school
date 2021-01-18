8ae01a48ea79a57d2b9a3914cae3406c499d164c
Stephanie <stephanie.furlan94@gmail.com>
2021-01-18 11:38:53 +0100

    Jumbotron and header

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
-stickyElem = document.querySelector("#mainHeader");
-x = document.querySelector("#page-content");
+var stickyHeader = document.querySelector("#main-header");
+var menu = document.querySelector("#main-header__content");
 
-   /* Gets the amount of height
-   of the element from the
-   viewport and adds the
-   pageYOffset to get the height
-   relative to the page */
-   currStickyPos = stickyElem.getBoundingClientRect().top + window.pageYOffset;
+/* Gets the amount of height
+of the element from the
+viewport and adds the
+pageYOffset to get the height
+relative to the page */
+var currHeaderPos = stickyHeader.getBoundingClientRect().top + window.pageYOffset;
 
-   window.onscroll = function() {
+window.onscroll = function() {
 
-      /* Check if the current Y offset
-      is greater than the position of
-      the element */
-      if (window.pageYOffset > currStickyPos) {
-          stickyElem.classList.add('header-small');
-          stickyElem.classList.remove('header-big');
-          // x.classList.add('page-up');
-      } else {
-         stickyElem.classList.add('header-big');
-         stickyElem.classList.remove('header-small');
-         // x.classList.remove('page-up');
-      }
+   /* Check if the current Y offset
+   is greater than the position of
+   the element */
+   if (window.pageYOffset > currHeaderPos) {
+       stickyHeader.classList.add('header-small');
+       stickyHeader.classList.remove('header-big');
+       menu.classList.add('menu-small');
+       menu.classList.remove('menu-big');
+   } else {
+      stickyHeader.classList.add('header-big');
+      stickyHeader.classList.remove('header-small');
+      menu.classList.add('menu-big');
+      menu.classList.remove('menu-small');
    }
+}
